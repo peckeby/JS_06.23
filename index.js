690032ec525881657f1ede965c7001c5f5bae578
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2023-06-05 15:05:27 +0300

    Done task 1,2,3

--- index.js
+++ index.js
@@ -1,68 +1,151 @@
-function getDistance(x1, y1, x2, y2) {
-  if (arguments.length !== 4) {
-    throw new Error();
-  }
-  for (let arg of arguments) {
-    if (arg > 1000 || arg < -1000) {
-      throw new Error();
-    }
-    if (arg === Infinity || arg === -Infinity || arg === NaN) {
-      throw new Error();
-    }
-
-    if (arg == undefined) {
-      throw new Error();
-    }
-  }
-  const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  return distance;
+function isPrimitive(value) {
+  return value !== Object(value);
 }
 
-//==============================TASK 2====================================//
+function isObject(obj) {
+  return obj?.constructor === Object;
+} //используется в трех задачах!
 
-function switchPlaces(arr) {
-  if (Array.isArray(arr) === false || arr == undefined) {
-    throw new Error();
-  }
-
-  if (arr.length === 0) {
-    return arr;
-  }
-
-  let result = [];
-
-  for (let i = Math.round(arr.length / 2); i < arr.length; i++) {
-    result.push(arr[i]);
-  }
-  if (arr.length % 2 === 0) {
-    for (let i = 0; i < Math.round(arr.length / 2); i++) {
-      result.push(arr[i]);
+function reduceComplexObjects(writer, acc, value, ...args) {
+  if (!isObject(value)) {
+    switch (value.constructor) {
+      case Array:
+        writer(deepCopyArray, acc, value, ...args);
+        break;
+      case Map:
+        writer(deepCopyMap, acc, value, ...args);
+        break;
+      case Set:
+        writer(deepCopySet, acc, value, ...args);
+        break;
     }
   } else {
-    result.push(arr[Math.floor(arr.length / 2)]);
-    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-      result.push(arr[i]);
-    }
+    writer(deepCopy, acc, value, ...args);
   }
-  return result;
+  return acc;
 }
 
-//==============================TASK 3====================================//
+function reducer(writer, acc, value, ...args) {
+  if (!isPrimitive(value)) {
+    return reduceComplexObjects(writer, acc, value, ...args);
+  } else {
+    writer((v) => v, acc, value, ...args);
+    return acc;
+  }
+}
 
-function getDivisors(number) {
-  if (
-    number === Infinity ||
-    number === -Infinity ||
-    number === NaN ||
-    number == undefined
-  ) {
+function arrayWriter(copyFunction, acc, value) {
+  acc.push(copyFunction(value));
+}
+
+function objectWriter(copyFunction, acc, value, key) {
+  acc[key] = copyFunction(value);
+}
+
+function setWriter(copyFunction, acc, value) {
+  acc.add(copyFunction(value));
+}
+
+function mapWriter(copyFunction, acc, value, key) {
+  if (isPrimitive(key)) {
+    acc.set(key, copyFunction(value));
+  } else {
+    const [keyCopy] = reduceComplexObjects(arrayWriter, [], key);
+    acc.set(keyCopy, copyFunction(value));
+  }
+}
+
+function deepCopyArray(array) {
+  return array.reduce((acc, value) => {
+    return reducer(arrayWriter, acc, value);
+  }, []);
+}
+
+function deepCopyMap(map) {
+  return [...map.entries()].reduce((acc, [key, value]) => {
+    return reducer(mapWriter, acc, value, key);
+  }, new Map());
+}
+
+function deepCopySet(set) {
+  return [...set.values()].reduce((acc, value) => {
+    return reducer(setWriter, acc, value);
+  }, new Set());
+}
+
+function deepCopy(objectToCopy) {
+  return Object.entries(objectToCopy).reduce((acc, [key, value]) => {
+    return reducer(objectWriter, acc, value, key);
+  }, {});
+}
+
+export function makeDeepCopy(obj) {
+  if (!isObject(obj)) {
     throw new Error();
   }
-  let divisors = [];
-  for (i = number; i >= 1; i--) {
-    if (number % i === 0) {
-      divisors.push(i);
-    }
+  return deepCopy(obj);
+}
+
+//=====================================================Task 2============================================================///
+
+function createIterable(x, y) {
+  if (!Number.isInteger(x) || !Number.isInteger(y)) {
+    throw new Error();
   }
-  return divisors;
+  if (x >= y) {
+    throw new Error();
+  }
+
+  return {
+    from: x,
+    to: y,
+    [Symbol.iterator]() {
+      let i = this.from - 1;
+      const end = this.to + 1;
+      return {
+        next() {
+          i++;
+          if (i === end) {
+            return { done: true, value: i };
+          } else {
+            return { done: false, value: i };
+          }
+        },
+      };
+    },
+  };
+}
+
+//=====================================================Task 3============================================================///
+
+function createProxy(obj) {
+  if (!isObject(obj)) {
+    throw new Error();
+  }
+
+  const handler = {
+    get(target, prop) {
+      const entry = target[prop];
+
+      if (entry) {
+        entry.readAmount++;
+        return entry.value;
+      } else {
+        return;
+      }
+    },
+    set(target, prop, value) {
+      const entry = target[prop];
+
+      if (entry) {
+        if (typeof value === typeof entry.value || value === null) {
+          entry.value = value;
+        }
+      } else {
+        target[prop] = { value, readAmount: 0 };
+      }
+      return true;
+    },
+  };
+  return new Proxy(obj, handler);
 }
